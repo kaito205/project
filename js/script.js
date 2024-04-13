47b65d50902ea19102bed45bfeff97069e3894d2
kaito205 <bochilandi85@gmail.com>
2024-04-13 18:18:46 +0700

    initial

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Toggel class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika menu diklik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar saidbar untuk menghilangkan nav
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Function untuk menangani klik pada ikon log-in
+function redirectToLogin() {
+  // Gantilah URL berikut dengan URL halaman login Anda
+  window.location.href = "js/login.html";
+}
+
+// Menambahkan event listener pada ikon log-in
+document.getElementById("log-in").addEventListener("click", redirectToLogin);
